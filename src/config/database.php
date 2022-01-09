a7984cb2d190e0afb0935578469829c5861c0fe9
Linhchi2012 <94341441+Linhchi2012@users.noreply.github.com>
2022-01-09 12:34:16 +0700

    Add files via upload

---
+++ src/config/database.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 07/01/2022
+ * Time: 11:36 PM
+ */
+//khai báo các hằng số dùng cho kết nối csdl
+const DB_HOST = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = '1951060553_libraries';
+?>
